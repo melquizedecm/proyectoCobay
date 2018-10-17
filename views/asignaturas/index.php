f078c7f13f5ab9fea05655a6378c889f965f6a38
Aaron Pech <aaronpech13@outlook.com>
2018-10-17 07:53:17 -0500

    Aaron Pech Heredia :P

---
+++ views/asignaturas/index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<html>
+    <head>
+
+    </head>
+    <body>
+
+        <div class="container center-block" >
+            <form class="form-horizontal" name="formAsignatura">
+                <fieldset>
+                <legend align="center">Asignaturas</legend>
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Text Input</label>  
+                <div class="col-md-4">
+                    <input name="textinput" class="form-control input-md" id="textinput" type="text" placeholder="placeholder">
+                    <span class="help-block">help</span>  
+                </div>
+            </div>
+
+    </body>
+</html>
