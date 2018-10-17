79d71451ae63370a321d1413af72c563675ebac0
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2018-10-17 17:03:33 -0500

    calificaciones

--- views/calificaciones/index.php
+++ views/calificaciones/index.php
@@ -167,7 +167,7 @@
                 <div class="table-title">
                     <div class="row">
 
-                        <div class="col-sm-8"><h2>Calificaciones <b></b></h2></div>
+                        <div class="col-sm-8"><h2>Calificaciones de alumnos <b></b></h2></div>
                         <div class="col-sm-4">
                             <form class="md-form">
                                 <div class="file-field">
