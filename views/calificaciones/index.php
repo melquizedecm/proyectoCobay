4d4e38e485dca6f1e6a57c38aaf1f683dff31a57
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2018-10-20 10:48:33 -0500

    calificaciones (intentando acomodar)

--- views/calificaciones/index.php
+++ views/calificaciones/index.php
@@ -19,6 +19,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <style type="text/css">
+    
     body {
         color: #404E67;
         background: #F5F7FA;
@@ -103,37 +104,39 @@
         display: none;
     }
 </style>
-<script type="text/javascript">
+            <script type="text/javascript">
     $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-        var actions = $("table td:last-child").html();
-        // Append table with add row form on add new button click
-        $(".add-new").click(function () {
-            $(this).attr("disabled", "disabled");
+    $('[data-toggle="tooltip"]').tooltip();
+            var actions = $("table td:last-child").html();
+            // Append table with add row form on add new button click
+    $(".add-new").click(function () {
+    $(this).attr("disabled", "disabled");
             var index = $("table tbody tr:last-child").index();
             var row = '<tr>' +
-                    '<td><input type="text" class="form-control" name="name" id="name"></td>' +
-                    '<td><input type="text" class="form-control" name="department" id="department"></td>' +
-                    '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-                    '<td>' + actions + '</td>' +
-                    '</tr>';
-            $("table").append(row);
-            $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-        // Add row on add button click
-        $(document).on("click", ".add", function () {
-            var empty = false;
-            var input = $(this).parents("tr").find('input[type="text"]');
-            input.each(function () {
-                if (!$(this).val()) {
-                    $(this).addClass("error");
-                    empty = true;
-                } else {
-                    $(this).removeClass("error");
-                }
-            });
-            $(this).parents("tr").find(".error").first().focus();
+            '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+            '<td><input type="text" class="form-control" name="matricula" id="matricula"></td>' +
+            '<td><input type="text" class="form-control" name="parcial 1" id="parcial 1"></td>' +
+            '<td><input type="text" class="form-control" name="parcial 2" id="parcial 2"></td>' +
+            '<td><input type="text" class="form-control" name="ordinario" id="ordinario"></td>' +
+            '<td><input type="text" class="form-control" name="promedio" id="promedio"></td>' + <!--'<td>' + actions + '</td>' +-->
+        '</tr>';
+                        $("table").append(row);
+                        $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+                        $('[data-toggle="tooltip"]').tooltip();
+                });
+                        // Add row on add button click
+                        $(document).on("click", ".add", function () {
+                var empty = false;
+                        var input = $(this).parents("tr").find('input[type="text"]');
+                        input.each(function () {
+                        if (!$(this).val()) {
+                        $(this).addClass("error");
+                                empty = true;
+                        } else {
+                        $(this).removeClass("error");
+                        }
+                        });
+  $(this).parents("tr").find(".error").first().focus();
             if (!empty) {
                 input.each(function () {
                     $(this).parent("td").html($(this).val());
@@ -162,117 +165,120 @@
 <body>
 
     <div class="container">
-        <center>
-            <div class="table-wrapper">
-                <div class="table-title">
-                    <div class="row">
 
-                        <div class="col-sm-8"><h2>Calificaciones de alumnos <b></b></h2></div>
-                        <div class="col-sm-4">
-                            <form class="md-form">
-                                <div class="file-field">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
 
-                                    <div class="btn btn-primary btn-sm float-left">
-                                        <span>Choose file</span>
-                                        <input type="file">
-                                    </div>
-                                    <div class="file-path-wrapper">
-                                        <input class="file-path validate" type="text" placeholder="Upload your file">
-                                    </div>
-                                </div>
-                            </form>
-                        </div>                    
-                    </div>
+                    <div class="col-sm-8"><center><h2>Calificaciones de alumnos <b></b></h2></center></div>
 
-                    </center>
-        <center>
-                    <div class="dropdown">
-                        
-                            <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Grupos
-                                <span class="caret"></span></button>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">1A</a></li>
-                                <li role="presentation" class="divider"></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">1B</a></li>
-                                <li role="presentation" class="divider"></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">1C</a></li>
+                </div>
+                <div class="col-sm-4" style="float:rigth;">
+                    <form class="md-form">
+                        <div class="file-field">
+
+                            <div class="btn btn-primary btn-sm float-left">
+                                <span>Choose file</span>
+                                <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text" placeholder="Upload your file">
+                            </div>
+                        </div>
+                    </form>
+                </div>     
+
+                <div style="position:absolute; top:80px; left:300px; right:0;">
+                <div class="dropdown" weigth="120px">
+
+                    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Grupos
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">1A</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">1B</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">1C</a></li>
 
 
-                            </ul>
-                        
-                    </div>
+                    </ul>
 
-                    <div class="dropdown">
-                        
-                            <button class="btn btn-default dropdown-toggle" type="button" id="menu2" data-toggle="dropdown">Materias
-                                <span class="caret"></span></button>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu2">
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Informatica</a></li>
-                                <li role="presentation" class="divider"></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Matematicas</a></li>
-                                <li role="presentation" class="divider"></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Ingles 1</a></li>
-                            </ul>
-                      
-                    </div>
-              </center>
-        
-                    <button type="button" class="btn btn-info"><i class="fa fa-plus"></i> Buscar</button>
-                    
                 </div>
 
-      
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Matricula</th>
-                    <th>Parcial 1</th>
-                    <th>Parcial 2</th>
-                    <th>Ordinario</th>
-                    <th>Promedio</th>
+                <div class="dropdown">
 
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Juan Carlos Rodriguez</td>
-                    <td>4160020B</td>
-                    <td>35</td>
-                    <td>35</td>
-                    <td>30</td>
-                    <td>100</td>
-                   
-                </tr>
-                <tr>
-                    <td>Carlos Castro</td>
-                    <td>4160003B</td>
-                    <td>35</td>
-                    <td>35</td>
-                    <td>30</td>
-                    <td>100</td>
-                </tr>
-                <tr>
-                    <td>Aaron Pech</td>
-                    <td>4160016B</td>
-                    <td>35</td>
-                    <td>35</td>
-                    <td>30</td>
-                    <td>100</td>
-                    
-                </tr>
-                <tr>
-                    <td>Nerstor Reina</td>
-                    <td>4160013B</td>
-                    <td>35</td>
-                    <td>35</td>
-                    <td>30</td>
-                    <td>100</td>
-                   
-                </tr> 
-            </tbody>
-        </table>
-    </div>
-</div>     
+                    <button class="btn btn-default dropdown-toggle" type="button" id="menu2" data-toggle="dropdown">Materias
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="menu2">
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Informatica</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Matematicas</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Ingles 1</a></li>
+                    </ul>
+
+                </div>
+                </div>
+                <div class="button" style="position:absolute; top:90; left:500;">
+                    <button type="button" class="btn btn-info"><i class="fa fa-plus"></i> Buscar</button>
+                </div>
+
+
+
+            </div>
+
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Matricula</th>
+                        <th>Parcial 1</th>
+                        <th>Parcial 2</th>
+                        <th>Ordinario</th>
+                        <th>Promedio</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Juan Carlos Rodriguez</td>
+                        <td>4160020B</td>
+                        <td>35</td>
+                        <td>35</td>
+                        <td>30</td>
+                        <td>100</td>
+
+                    </tr>
+                    <tr>
+                        <td>Carlos Castro</td>
+                        <td>4160003B</td>
+                        <td>35</td>
+                        <td>35</td>
+                        <td>30</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>Aaron Pech</td>
+                        <td>4160016B</td>
+                        <td>35</td>
+                        <td>35</td>
+                        <td>30</td>
+                        <td>100</td>
+
+                    </tr>
+                    <tr>
+                        <td>Nerstor Reina</td>
+                        <td>4160013B</td>
+                        <td>35</td>
+                        <td>35</td>
+                        <td>30</td>
+                        <td>100</td>
+
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
+    </div>     
 </body>
 
