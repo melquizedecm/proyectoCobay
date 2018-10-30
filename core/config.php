7a7685a0a416bc35564458f3c87e40794a60cc6b
melqui <melqui@localhost>
2018-10-30 07:24:40 -0600

    configuracion para localhost

--- core/config.php
+++ core/config.php
@@ -1,29 +1,30 @@
 <?php
-function conectar(){
+
+function conectar() {
 ///PARAMETROS PARA LOCALHOST
-$DBuser="root";
-$DBpass="";
-$DBserver="localhost";
-$DBdatos="proyectoCobay";
+    $DBuser = "root";
+    $DBpass = "";
+    $DBserver = "localhost";
+    $DBdatos = "proyecto_cobay";
 ////////////NUEVO METODO DE CONEXION////////////////////
-$link = mysqli_connect($DBserver,$DBuser,$DBpass,$DBdatos);
+    $link = mysqli_connect($DBserver, $DBuser, $DBpass, $DBdatos);
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	return $link;
-    exit;
-}
-else{
-	return $link;
-}
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        return $link;
+    } else {
+        return $link;
+    }
 }
 
-function liberar($resultado){
-	$resultado->free();
+function liberar($resultado) {
+    $resultado->free();
 }
-function cerrar($link){
-	$link->close();
+
+function cerrar($link) {
+    $link->close();
 }
+
 ?>
