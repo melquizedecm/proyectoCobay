aed46b119948f3f8d40b16e5b169b3cae32e9c75
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2018-10-30 08:16:30 -0600

    modal

--- views/calificaciones/index.php
+++ views/calificaciones/index.php
@@ -10,8 +10,6 @@
  * 2. Lista de alumnos con su calificación
  * 
  */
-
-
 ?>
 <!DOCTYPE html>
 <head>
@@ -252,7 +250,29 @@
                         </div>
                     </div>-->
                     <div class="col-sm-2" >
-                        <button type="button" class="btn btn-info"><i class="fa fa-plus"></i> Buscar Lista de Alumnos</button>
+                        <!--<button type="button" class="btn btn-info"><i class="fa fa-plus"></i> Buscar Lista de Alumnos</button>-->
+                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"></i> Buscar Lista de Alumnos</button>
+
+                        <!-- Modal -->
+                        <div id="myModal" class="modal fade" role="dialog">
+                            <div class="modal-dialog">
+
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 class="modal-title">Selección de Lista</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Some text in the modal.</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -274,8 +294,8 @@
                     </tr>
                 </thead>
                 <tbody>
-                    
-                    
+
+
                     <tr>
                         <td>1</td>
                         <td>Juan Carlos Rodriguez</td>
