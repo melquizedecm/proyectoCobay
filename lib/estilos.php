055bbd95e8f4d5dd0bb0e43b50b4850313263416
melqui <melqui@localhost>
2018-11-07 16:43:29 -0600

    Actualizacion de librerias /lib

--- lib/estilos.php
+++ lib/estilos.php
@@ -1,11 +1,9 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 function estilosMenu() {
     ?>
     <style>
@@ -119,7 +117,5 @@ function estilosPaginas() {
             display: none;
         }
     </style>
-
     <?php
-
 }
