55a3736f07a7b746977fa73329aae9020f29ba42
melqui <melqui@localhost>
2018-11-07 10:41:02 -0600

    Actualizacion de librerias

--- lib/meta.php
+++ lib/meta.php
@@ -1,31 +1,19 @@
 <?php
 
-class Meta {
+function getMeta($titulo) {
+    ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>COBAY - <? echo $titulo?></title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">  
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
-    function getMeta() {
-        ?>
-
-        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="Instituto Tecnológico Superior Progreso">
-        <meta name="description" content="Instituto Tecnológico Superior Progreso">
-        <meta name="keyword" content="gestion usuarios, lista usuarios">
-        <link rel="shortcut icon" href="../img/favicon.ico">
-
-        <title>Instituto Tecnológico Superior Progreso </title>
-
-        <link rel="stylesheet" href="../lib/css/color-itsp.css">
-        <link rel="stylesheet" href="../lib/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../lib/css/estilo.css">
-        <link rel="stylesheet" href="../lib/css/jquery.dataTables.min.css">
-        <script src="../lib/js/jquery.min.js"></script>
-        <script src="../lib/js/bootstrap.min.js"></script>
-        <script src="../lib/js/jquery-1.12.3.js"></script>
-        <script src="../lib/js/jquery.dataTables.min.js"></script>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <?php
-    }
-
+    <?php
 }
