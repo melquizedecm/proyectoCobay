f7ac4c23c5b12f1f958b01274d2e8576d28f3139
Angel <ing.agarcia1803@gmail.com>
2018-11-08 08:36:32 -0600

    Llamar a estilos y datatable

--- views/periodos/index.php
+++ views/periodos/index.php
@@ -18,109 +18,22 @@ require_once ('../../models/Periodos.php');
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PERIODOS</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <style type="text/css">
-            body {
-                color: #404E67;
-                background: #F5F7FA;
-                font-family: 'Open Sans', sans-serif;
-            }
-            .table-wrapper {
-                width: 700px;
-                margin: 30px auto;
-                background: #fff;
-                padding: 20px;	
-                box-shadow: 0 1px 1px rgba(0,0,0,.05);
-            }
-            .table-title {
-                padding-bottom: 10px;
-                margin: 0 0 10px;
-            }
-            .table-title h2 {
-                margin: 6px 0 0;
-                font-size: 22px;
-            }
-            .table-title .add-new {
-                float: right;
-                height: 30px;
-                font-weight: bold;
-                font-size: 12px;
-                text-shadow: none;
-                min-width: 100px;
-                border-radius: 50px;
-                line-height: 13px;
-            }
-            .table-title .add-new i {
-                margin-right: 4px;
-            }
-            table.table {
-                table-layout: fixed;
-            }
-            table.table tr th, table.table tr td {
-                border-color: #e9e9e9;
-            }
-            table.table th i {
-                font-size: 13px;
-                margin: 0 5px;
-                cursor: pointer;
-            }
-            table.table th:last-child {
-                width: 100px;
-            }
-            table.table td a {
-                cursor: pointer;
-                display: inline-block;
-                margin: 0 5px;
-                min-width: 24px;
-            }    
-            table.table td a.add {
-                color: #27C46B;
-            }
-            table.table td a.edit {
-                color: #FFC107;
-            }
-            table.table td a.delete {
-                color: #E34724;
-            }
-            table.table td i {
-                font-size: 19px;
-            }
-            table.table td a.add i {
-                font-size: 24px;
-                margin-right: -1px;
-                position: relative;
-                top: 3px;
-            }    
-            table.table .form-control {
-                height: 32px;
-                line-height: 32px;
-                box-shadow: none;
-                border-radius: 2px;
-            }
-            table.table .form-control.error {
-                border-color: #f50000;
-            }
-            table.table td .add {
-                display: none;
-            }
-        </style>
+        
+        <?php
+        getMeta("Administración de Periodos");
+        estilosPaginas();
+        ?>        
         <script type="text/javascript">
+
+            ///////DATABLES ////////
             $(document).ready(function ()
             {
-                ///////DATABLES ////////
-                $(document).ready(function ()
-                {
-                    $('#tablaPeriodos').DataTable();
-                });
+                $('#tablaPeriodos').DataTable();
+            });
+
+            $(document).ready(function ()
+            {
+
 
                 //GENERACION DEL CRUD EN LA TABLA
                 $('[data-toggle="tooltip"]').tooltip();
@@ -221,9 +134,9 @@ require_once ('../../models/Periodos.php');
     </head>
     <body>
 
-<?php
-getHeader();
-?>
+        <?php
+        getHeader();
+        ?>
 
         <div class="container">
             <div class="table-wrapper">
@@ -246,24 +159,25 @@ getHeader();
                     </thead>
                     <tbody>
 
-<?php
-$json = $periodos->read();
-$datosTabla = json_decode($json);
+                        <?php
+                        $json = $periodos->read();
+                        $datosTabla = json_decode($json);
 
-foreach ($datosTabla as $row) {
-    echo "<tr><td>" . $row->{'id_periodo'} . "</td>"
-    . "<td>" . $row->{'periodo'} . "</td>";
-    if ($row->{'status_periodo'} === "ACTIVO") {
-        echo "<td><button class='btn-success'>     " . $row->{'status_periodo'} . "</button></td>";
-    } else {
-        echo "<td><button class='btn-danger'>     " . $row->{'status_periodo'} . "</button></td>";
-    }
-    echo "<td><a class = 'add' title = 'Agregar' data-toggle = 'tooltip'><i class = 'material-icons'>&#xE03B;</i></a>"
-    . "<a class = 'edit' title = 'Editar' data-toggle = 'tooltip'><i class = 'material-icons'>&#xE254;</i></a>"
-    . "<a class = 'delete' title = 'Eliminar' data-toggle = 'tooltip'><i class = 'material-icons'>&#xE872;</i></a>"
-    . "</td></tr>";
-}
-?>
+                        foreach ($datosTabla as $row) {
+                            echo "<tr><td>" . $row->{'id_periodo'} . "</td>"
+                            . "<td>" . $row->{'periodo'} . "</td>";
+                            if ($row->{'status_periodo'} === "ACTIVO") {
+                                echo "<td><button class='btn-success'>" . $row->{'status_periodo'} . "</button></td>";
+                            } else {
+                                echo "<td><button class='btn-danger'>" . $row->{'status_periodo'} . "</button></td>";
+                            }
+
+                            echo "<td><a class = 'add' title = 'Agregar' data-toggle = 'tooltip'><i class = 'material-icons'>&#xE03B;</i></a>"
+                            . "<a class = 'edit' title = 'Editar' data-toggle = 'tooltip'><i class = 'material-icons'>&#xE254;</i></a>"
+                            . "<a class = 'delete' title = 'Eliminar' data-toggle = 'tooltip'><i class = 'material-icons'>&#xE872;</i></a>"
+                            . "</td></tr>";
+                        }
+                        ?>
 
                     </tbody>
                 </table>
