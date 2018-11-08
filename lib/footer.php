24caf3d51d4f391e981e2a31b6178977f4359c34
JuanCRB <juanchocarlos98@hotmail.com>
2018-11-08 09:03:45 -0600

    cambio de imagenes

--- lib/footer.php
+++ lib/footer.php
@@ -6,10 +6,10 @@
                 <div class="row">
                     <div class="col-xs-12 inner">
                         <ul class="icons">
-                        <h4><img src="../../img/laptop.png"<li><a href="http://www.cobay.edu.mx/" class="icon alt fa-twitter"><span class="label">Pagina Oficial</h4></span></a></li>
+                        <h4><img src="../../img/navegador.png"<li><a href="http://www.cobay.edu.mx/" class="icon alt fa-twitter"><span class="label">Pagina Oficial</h4></span></a></li>
                         <h4><img src="../../img/facebook.png"<li><a href="https://www.facebook.com/COBAYPlantelProgreso/" class="icon alt fa-twitter"><span class="label">Facebook</h4></span></a></li>
                         <h4><img src="../../img/arroba.png"<li><span class="label">Correo: cobay@cobay.edu.mx</h4></span></a></li>
-                        <h4><img src="../../img/telefono.png"<li><span class="label">Telefono: (999) 611.86.90</h4></span></a></li>
+                        <h4><img src="../../img/whatsapp.png"<li><span class="label">Telefono: (999) 611.86.90</h4></span></a></li>
                     </div>
                 </div>
             </div>
