fcde841b001852d42e34978aefd8fb7a1682c73e
melqui <melqui@localhost>
2018-11-08 13:02:31 -0600

    cambios a footer

--- lib/footer.php
+++ lib/footer.php
@@ -1,19 +1,19 @@
 <?php
-    function getFooter() {
-        ?>
-                <
-            <div id="footer" class="container-fluid text-center">
-                <div class="row">
-                    <div class="col-xs-12 inner">
-                        <ul class="icons">
-                        <h4><img src="../../img/navegador.png"<li><a href="http://www.cobay.edu.mx/" class="icon alt fa-twitter"><span class="label">Pagina Oficial</h4></span></a></li>
-                        <h4><img src="../../img/facebook.png"<li><a href="https://www.facebook.com/COBAYPlantelProgreso/" class="icon alt fa-twitter"><span class="label">Facebook</h4></span></a></li>
-                        <h4><img src="../../img/arroba.png"<li><span class="label">Correo: cobay@cobay.edu.mx</h4></span></a></li>
-                        <h4><img src="../../img/whatsapp.png"<li><span class="label">Telefono: (999) 611.86.90</h4></span></a></li>
-                    </div>
-                </div>
-            </div>
-        
-        <?php
-    }
 
+function getFooter() {
+    ?>
+    <
+    <div id="footer" class="container-fluid text-center">
+        <div class="row">
+            <div class="col-xs-12 inner">
+                <ul class="icons">
+                    <li><h5><img src="../../img/navegador.png"><a href="http://www.cobay.edu.mx/" class="icon alt fa-twitter"><span class="label">Pagina Oficial</span></a>
+                        <img src="../../img/facebook.png"><a href="https://www.facebook.com/COBAYPlantelProgreso/" class="icon alt fa-twitter"><span class="label">Facebook</span></a>
+                        <img src="../../img/arroba.png"><span class="label">Correo: cobay@cobay.edu.mx</span></a>
+                        <img src="../../img/whatsapp.png"><span class="label">Telefono: (999) 611.86.90</span></a></h5></li>
+            </div>
+        </div>
+    </div>
+
+    <?php
+}
