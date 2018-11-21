da743fb19180fb0a6973f2742c70c4207a17ecb7
AlejandroReyna96 <alejandro-psn@hotmail.com>
2018-11-21 09:08:59 -0600

    login- SESSION START

--- controllers/login.php
+++ controllers/login.php
@@ -1,14 +1,27 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-$matricula=$_POST['inputMatricula'];
-$password=$_POST['inputPassword'];
-
-
 session_start();
-$_SESSION['username']=$matricula;
+$matricula=$_POST['inputMatricula'];
+$contraseña=$_POST['inputPassword'];
+
+$_SESSION['username']=$matricula; 
+
+//Conectando a la base de datos
+$conexion=mysqli_connect("localhost","root","","proyecto_cobay");
+//Consulta
+
+$consulta= "SELECT * FROM alumnos WHERE matricula='".$matricula."' and password='".$contraseña."'"; 
+
+$resultado=  mysqli_query($conexion, $consulta);
+$filas=  mysqli_num_rows($resultado);
+
+if($filas>0)
+{
+    header("location:http://localhost/proyectoCobay/views/calificaciones/ConsultarCalificaciones.php");
+}
+else
+{
+    echo"Error en la autentificación";
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+?>
