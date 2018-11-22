242b78d90cafbf2a259f0640d898f1e56d67dd1d
AlejandroReyna96 <alejandro-psn@hotmail.com>
2018-11-21 19:05:20 -0600

    login- evitar inyección sql con addslashes

--- controllers/login.php
+++ controllers/login.php
@@ -1,28 +1,25 @@
 <?php
-session_start();
-$matricula= stripslashes($_POST['inputMatricula']);
-$contraseña=stripslashes($_POST['inputPassword']);
 
-$_SESSION['username']=$matricula; 
+session_start();
+$matricula =  addslashes($_POST['inputMatricula']);
+$contraseña = addslashes($_POST['inputPassword']);
+
+$_SESSION['username'] = $matricula;
 
 //Conectando a la base de datos
-$conexion=mysqli_connect("localhost","root","","proyecto_cobay");
+$conexion = mysqli_connect("localhost", "root", "", "proyecto_cobay");
 //Consulta
 
 
-$consulta= "SELECT * FROM administrativos WHERE matricula='".$matricula."' and password='".$contraseña."'"; 
+$consulta = "SELECT * FROM administrativos WHERE matricula='" . $matricula . "' and password='" . $contraseña . "'";
 
-$resultado=  mysqli_query($conexion, $consulta);
-$filas=  mysqli_num_rows($resultado);
+$resultado = mysqli_query($conexion, $consulta);
+$filas = mysqli_num_rows($resultado);
 
-if($filas>0)
-{
+if ($filas > 0) {
     header("location:http://localhost/proyectoCobay/views/Menu/");
-}
-else
-{
+} else {
     echo"Error en la autentificación";
-    
 }
 mysqli_free_result($resultado);
 mysqli_close($conexion);
