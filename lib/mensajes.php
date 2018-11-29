a13cdbdc460416f567fd54e8308355826e842988
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2018-11-29 08:22:14 -0600

    tratando de hacer algo

--- lib/mensajes.php
+++ lib/mensajes.php
@@ -1,21 +1,43 @@
 <?php
+/*
+ * FUNCION IMPRIMIR MENSAJE:
+ * 
+ * recibe en GET:
+ *     message =  "Mensaje a mostrar"
+ *     tipo    = 
+ *                 001 - Sucess -  Exito en la transaccion
+ *                 002 - Info -    Información Adicional
+ *                 003 - warning - Advertencia
+ *                 004 - Danger  - Peligro o Error en la transaccion.
+ */
 
-function imprimirMensaje($message) {
-    /*
-     * FUNCION IMPRIMIR MENSAJE:
-     * 
-     * recibe en GET:
-     *     message =  "Mensaje a mostrar"
-     *     tipo    = 
-     *                 001 - Sucess -  Exito en la transaccion
-     *                 002 - Info -    Información Adicional
-     *                 003 - warning - Advertencia
-     *                 004 - Danger  - Peligro o Error en la transaccion.
-     */
+function imprimirMensaje($message, $valor) {
 
-    if (!empty($message)) {
-        $messageType = $message[0];
-        $messageContent = $message[1];
+    if (!empty($valor)) {
+        $messageType = $message;
+        $messageContent = "";
+        //plantel
+        ($valor == "1") ? $messageContent = "Error con el plantel del excel" : '';
+        //periodo
+        ($valor == "2") ? $messageContent = "Error con el periodo del excel" : '';
+        //plan
+        ($valor == "3") ? $messageContent = "Error con el plan del excel" : '';
+        //semestre
+        ($valor == "4") ? $messageContent = "Error con el grupo del excel" : '';
+        //matricula profe
+        ($valor == "5") ? $messageContent = "Error con la matricula maestro del excel" : '';
+        //grupo
+        ($valor=="6")? $messageContent="Error con el grupo del excel":'';
+        //matricula asignatura
+        ($valor == "7") ? $messageContent = "Error con la clave de la asignatura del excel" : '';
+        //nombre asignatura
+        ($valor == "8") ? $messageContent = "Error con el nombre de la asignatura del excel" : '';
+        //matricula alumno
+        ($valor == "9") ? $messageContent = "Error con la matricula del alumno del excel" : '';
+        //nombre alumno
+        ($valor == "10") ? $messageContent = "Error con el nombre del alumno del excel" : '';
+
+
         if ($messageType == "001") {
             $messageClass = "alert alert-success";
         } elseif ($messageType == "002") {
@@ -37,3 +59,4 @@ function imprimirMensaje($message) {
 
 /////////////FINALIZA LA IMPRESIÓN DEL MENSAJE /////////////////
     
+
