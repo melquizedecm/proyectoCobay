b0653660662d487df3744966ede07664657bdcdc
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2018-12-11 09:11:01 -0600

    subir excel maestro

--- maestro-bwsw/models/Calificaciones.php
+++ maestro-bwsw/models/Calificaciones.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /*
@@ -14,25 +13,29 @@
  */
 require_once '../lib/consultas.php';
 require_once '../lib/mensajes.php';
+require_once '../models/Periodos.php';
+
 
 class Calificaciones {
 
     //put your code here
 
     function excelValido($data) {
+        $objPeriodo = new Periodos();
         $id_plantel = $data[0];
         $periodo = $data[1];
         $plan = $data[2];
-        $id_semestre = $data[3];
+        //$id_semestre = $data[3];
         $matricula_maestro = $data[4];
         $grupo = $data[5];
         $id_asignatura = $data[6];
         $asignatura = $data[7];
         $matricula = $data[8];
         $nombre = $data[9];
-        $parcial_uno = $data[10];
-        $parcial_dos = $data[11];
-        $ordinario = $data[12];
+        //echo $nombre;
+        //$parcial_uno = $data[10];
+        //$parcial_dos = $data[11];
+        //$ordinario = $data[12];
         $error = [];
         //SECCION PARA CONFIGURAR DATOS
 
@@ -46,8 +49,8 @@ class Calificaciones {
         $respuesta = getFilaSql("planes", "plan", $plan);
         ($respuesta == true) ? '' : array_push($error, "3");
         //semestre
-        $respuesta = getFilaSql("grupos", "id_grupo", $grupo);
-        ($respuesta == true) ? '' : array_push($error, "4");
+        //$respuesta = getFilaSql("grupos", "id_grupo", $grupo);
+        //($respuesta == true) ? '' : array_push($error, "4");
         //matricula de maestro
         $respuesta = getFilaSql("maestros", "matricula_maestro", $matricula_maestro);
         ($respuesta == true) ? '' : array_push($error, "5");
@@ -67,16 +70,15 @@ class Calificaciones {
         $respuesta = "SELECT * FROM alumnos WHERE nombre='".$nombre."'";
         $res1 = getFila($respuesta);
         
-        
+        //echo $res1['nombre'];
         ($res1['nombre'] == $nombre) ? '' : array_push($error, "10");
+         $periodoValido = $objPeriodo->esActivo($periodo);
+        ($periodoValido==true)? '':array_push($error,"11");
         
-       
         $banderaError = "";
         foreach ($error as $valor) {
             if ($valor != "") {
                 $banderaError = $valor;
-                $mensaje = "004";
-                imprimirMensaje($mensaje, $valor);
             }
         }
         return $banderaError;
